d1e0832255e8ebccd0b99a6e28e4cc7febf6ed27
mrDEVGRU <mrdevgru@gmail.com>
2023-05-23 16:02:20 +0400

    Create NewArray metod

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void NewArray(string[] array, string[] newArray)
+{
+    int count = 0;
+    int maxSymbol = 3;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= maxSymbol)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+    }
+}
+
+
