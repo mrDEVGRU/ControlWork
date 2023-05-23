23256fd0d481f263820f0baca1d7724c18fba8fa
mrDEVGRU <mrdevgru@gmail.com>
2023-05-23 19:20:39 +0400

    Пофиксил лишние элементы, осталась последняя запятая

--- Program.cs
+++ Program.cs
@@ -12,16 +12,31 @@
     }
 }
 
+int NewSize(string[] array, int size)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        if (array[j].Length <= 3)
+        {
+            size++;
+        }
+    }
+    return size;
+}
+
 void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(array[i] + ", ");
+        Console.Write($"{array[i]} ");
     }
 }
 
+int size = 0;
 string[] array = new string[] { "Hello", "2", "world", ":-)" };
 string[] fixArray = new string[array.Length];
 
+size = NewSize(array, size);
 NewArray(array, fixArray);
+Array.Resize(ref fixArray, size);
 PrintArray(fixArray);
